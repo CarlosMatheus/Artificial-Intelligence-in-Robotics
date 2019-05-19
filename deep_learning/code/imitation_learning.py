32067356b1eef236ee3065d2d36195be8781ac2b
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-05-19 18:36:02 -0300

    Adjust neural network

--- deep_learning/code/imitation_learning.py
+++ deep_learning/code/imitation_learning.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from utils import sum_gt_zero, xor
-from keras import models, layers, losses, optimizers, activations, regularizers, metrics
+from keras import models, layers, losses, optimizers, activations, metrics
 from math import pi
+import os
 
 # Joints' order in the dataset
 joints_dict = dict()
@@ -30,6 +30,7 @@ joints_dict['rightShoulderRoll'] = 19
 right_leg_joints = ['rightHipRoll', 'rightHipPitch', 'rightKneePitch', 'rightAnklePitch', 'rightAnkleRoll']
 
 num_epochs = 30000  # number of epochs for training
+
 # Figure format used for saving figures
 fig_format = 'png'
 # fig_format = 'svg'
@@ -40,24 +41,13 @@ positions = np.loadtxt('positions.txt')
 # The dataset contains the walking cycles, but we will use only the first one for training
 expected_output = positions[0:40, :]
 
-print(expected_output)
-
 # Creating a input vector (0.008 ms is the sample time of the walking algorithm)
 input = np.matrix(0.008 * np.arange(0, expected_output.shape[0])).T
 
 # Setting the random seed of numpy's random library for reproducibility reasons
 np.random.seed(0)
 
-
-
-
-
-
-
 alpha = 0.01
-lambda_l2 = 0.000  # lambda parameter of the L2 regularization
-# lambda_l2 = 0.002  # lambda parameter of the L2 regularizati/on
-
 num_cases = expected_output.shape[0]
 
 # Creates the neural network model in Keras
@@ -68,44 +58,17 @@ model = models.Sequential()
 # 'activation' configures the activation function
 # input_shape represents the size of the input
 # kernel_regularizer configures regularization for this layer
-model.add(layers.Dense(75, activation=activations.linear, input_shape=(1,),
-                       kernel_regularizer=regularizers.l2(lambda_l2)))
+model.add(layers.Dense(75, activation=activations.linear, input_shape=(1,)))
 model.add(layers.LeakyReLU(alpha))
-model.add(layers.Dense(50, activation=activations.linear, input_shape=(75,),
-                       kernel_regularizer=regularizers.l2(lambda_l2)))
+model.add(layers.Dense(50, activation=activations.linear, input_shape=(75,)))
 model.add(layers.LeakyReLU(alpha))
-model.add(layers.Dense(20, activation=activations.linear, input_shape=(50,),
-                       kernel_regularizer=regularizers.l2(lambda_l2)))
-
+model.add(layers.Dense(20, activation=activations.linear, input_shape=(50,)))
 model.compile(optimizer=optimizers.Adam(), loss=losses.mean_squared_error, metrics=[metrics.binary_accuracy])
 
 history = model.fit(input, expected_output, batch_size=num_cases, epochs=num_epochs)
 
-
-
-
-
-
-
-
-
-
-# input_predict = np.matrix(np.arange(0, input[-1] + 0.001, 0.001)).T
-
-input_predict = input
-
-output = model.predict(input_predict)  # add this line to predict the output from the Neural Network
-
-# output = np.zeros((len(input_predict), np.size(expected_output, 1)))  # remove this line
-
-
-
-
-
-
-
-
-
+input_predict = np.matrix(np.arange(0, input[-1] + 0.001, 0.001)).T
+output = model.predict(input_predict)
 
 # Comparing original and copied joint trajectories to evaluate the imitation learning
 for joint in right_leg_joints:
@@ -117,5 +80,5 @@ for joint in right_leg_joints:
     plt.xlabel('Time (s)')
     plt.ylabel('Joint Position (°)')
     plt.legend(['Original', 'Neural Network'])
-    plt.savefig(joint + '.' + fig_format, format=fig_format)
+    plt.savefig(os.path.join('imitation_learning_result', joint + '.' + fig_format), format=fig_format)
 plt.show()
