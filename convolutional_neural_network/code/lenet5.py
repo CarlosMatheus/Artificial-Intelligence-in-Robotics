4c29ba2dd5e69c2bf6baefeaaaafd8162e0418e1
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-05-24 15:15:44 -0300

    Add initial code

---
+++ convolutional_neural_network/code/lenet5.py
@@ -0,0 +1,10 @@
+from keras import layers, activations
+from keras.models import Sequential
+
+
+def make_lenet5():
+    model = Sequential()
+
+    # Todo: implement LeNet-5 model
+
+    return model
