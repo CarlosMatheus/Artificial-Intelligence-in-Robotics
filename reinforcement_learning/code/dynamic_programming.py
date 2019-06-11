31b5d15b5dfd0b5458bf5ac9a24837068f66f3f9
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-11 18:05:52 -0300

    Crate value iteration

--- reinforcement_learning/code/dynamic_programming.py
+++ reinforcement_learning/code/dynamic_programming.py
@@ -62,6 +62,20 @@ def greedy_policy(grid_world, value, epsilon=1.0e-3):
     return policy
 
 
+def evaluate(grid_world, value, policy, dimensions):
+    new_value = np.copy(initial_value)
+
+    return new_value
+
+
+def diff_in_val_greater_than_epsilon(old_value, new_value, dimensions, epsilon):
+    for j in range(dimensions[0]):
+        for i in range(dimensions[1]):
+            if abs(new_value[i][j] - old_value[i][j]) < epsilon:
+                return False
+    return True
+
+
 def policy_evaluation(grid_world, initial_value, policy, num_iterations=10000, epsilon=1.0e-5):
     """
     Executes policy evaluation for a policy executed on a grid world.
@@ -79,19 +93,23 @@ def policy_evaluation(grid_world, initial_value, policy, num_iterations=10000, e
     :return: value function of the given policy.
     :rtype: bidimensional NumPy array.
     """
-    dimensions = grid_world.dimensions
-    value = np.copy(initial_value)
-    # Todo: implement policy evaluation.
-
-    print('policy_evaluation')
 
     """
     Intera chamando value interaion? 
     até convergir o value e entao retornando aquela policy evaluated
     
-    
-    
     """
+    # Todo: implement policy evaluation.
+
+    dimensions = grid_world.dimensions
+    value = np.copy(initial_value)
+
+    for i in range(num_iterations):
+        new_value = evaluate(grid_world, value, dimensions)
+        if not diff_in_val_greater_than_epsilon(value, new_value, dimensions, epsilon):
+            value = new_value
+        else:
+            break
 
     return value
 
@@ -111,29 +129,59 @@ def value_iteration(grid_world, initial_value, num_iterations=10000, epsilon=1.0
     :return value: optimal value function.
     :rtype value: bidimensional NumPy array.
     """
-    dimensions = grid_world.dimensions
-    value = np.copy(initial_value)
-    # Todo: implement value iteration.
 
     """
     executes just one value iteration???
-    
-    
     Will use the bellman equation
     the value iteration, 
     initialize all grid with -1, and end with 0
     iterate using bellman, not makaing recursion, but using the previos genaratiiin board
     after a large number of iterations, the values inside the grid will converge
     """
+    """
+    the CORRECT_ACTION_PROB and GAMMA are inside grid_world'
+    ja existe a funcao transition_probability => p(s'|s, a)
+    ja existe a funcao reward => r(s, a)
+    ja existe get_valid_sucessors
+    """
 
-    print('value_iteration')
-    for asdf in asdf:
-        print("asf")
+    # Todo: Test this function
+
+    dimensions = grid_world.dimensions
+    value = np.copy(initial_value)
+
+    possible_actions = [STOP, UP, RIGHT, DOWN, LEFT]
+
+    old_value = value
+    value = np.copy(old_value)
+
+    for _ in range(num_iterations):
+        for i in range(dimensions[0]):
+            for j in range(dimensions[1]):
+                current_state = (i, j)
+                max_val = -float('inf')
+                for action in possible_actions:
+                    rew = grid_world.reward(current_state, action)
+                    sucessors_states = grid_world.get_valid_sucessors(current_state)
+                    val_sum = 0
+                    for sucessor in sucessors_states:
+                        prob = grid_world.transition_probability(current_state, action, sucessor)
+                        val = old_value[sucessor[0]][sucessor[1]]
+                        val_sum += prob * val
+                    max_val = max(max_val, rew + grid_world.gamma * val_sum)
+
+                value[current_state[0]][current_state[1]] = max_val
+
+        if diff_in_val_greater_than_epsilon(old_value, value, dimensions, epsilon):
+            old_value = value
+            value = np.copy(old_value)
+        else:
+            break
 
     return value
 
 
-def check_policy(old_policy, new_policy):
+def is_policy_equal(old_policy, new_policy):
     return np.array_equal(old_policy, new_policy)
 
 
@@ -178,7 +226,7 @@ def policy_iteration(grid_world, initial_value, initial_policy, evaluations_per_
     for i in range(num_iterations):
         new_value = policy_evaluation(grid_world, value, policy)
         new_policy = greedy_policy(grid_world, new_value)
-        if not check_policy(policy, new_policy):
+        if not is_policy_equal(policy, new_policy):
             policy = new_policy
             value = new_value
         else:
