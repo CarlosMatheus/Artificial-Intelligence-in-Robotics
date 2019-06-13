efa7885a2f41c2b34ce3b84e68552eec19b7bd5f
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-06-13 16:25:27 -0300

    Remove some comments

--- reinforcement_learning/code/dynamic_programming.py
+++ reinforcement_learning/code/dynamic_programming.py
@@ -3,57 +3,6 @@ from math import inf, fabs
 from utils import *
 
 
-# def print_value(grid_world, value):
-#     """
-#     Prints a value function.
-#
-#     :param value: table (i, j) representing the value function.
-#     :type value: bidimensional NumPy array.
-#     """
-#     print('Value function:')
-#     dimensions = value.shape
-#     for i in range(dimensions[0]):
-#         print('[', end='')
-#         for j in range(dimensions[1]):
-#             state = (i, j)
-#             if grid_world.is_cell_valid(state):
-#                 print('%9.2f' % value[i, j], end='')
-#             else:
-#                 print('    *    ', end='')
-#             if j < dimensions[1] - 1:
-#                 print(',', end='')
-#         print(']')
-#
-#
-# def print_policy(grid_world, policy):
-#     """
-#     Prints a policy. For a given state, assumes that the policy executes a single action
-#     or a set of actions with the same probability.
-#
-#     :param policy: table (i, j, a) representing the policy.
-#     :type policy: tridimensional NumPy array.
-#     """
-#     print('Policy:')
-#     action_chars = 'SURDL'
-#     dimensions = policy.shape[0:2]
-#     for i in range(dimensions[0]):
-#         print('[', end='')
-#         for j in range(dimensions[1]):
-#             state = (i, j)
-#             if grid_world.is_cell_valid(state):
-#                 cell_text = ''
-#                 for action in range(NUM_ACTIONS):
-#                     if policy[i, j, action] > 1.0e-3:
-#                         cell_text += action_chars[action]
-#             else:
-#                 cell_text = '*'
-#             cell_text = cell_text.center(9)
-#             print(cell_text, end='')
-#             if j < dimensions[1] - 1:
-#                 print(',', end='')
-#         print(']')
-
-
 def random_policy(grid_world):
     """
     Creates a random policy for a grid world.
@@ -178,11 +127,7 @@ def policy_evaluation(grid_world, initial_value, policy, num_iterations=10000, e
     :return: value function of the given policy.
     :rtype: bidimensional NumPy array.
     """
-    """
-    Intera chamando value interaion? 
-    até convergir o value e entao retornando aquela policy evaluated
-    """
-    # Todo: Test
+
     dimensions = grid_world.dimensions
     value = evaluate(grid_world, initial_value, policy, dimensions, num_iterations, epsilon)
     return value
@@ -203,18 +148,7 @@ def value_iteration(grid_world, initial_value, num_iterations=10000, epsilon=1.0
     :return value: optimal value function.
     :rtype value: bi-dimensional NumPy array.
     """
-    """
-    executes just one value iteration???
-    Will use the bellman equation
-    the value iteration, 
-    initialize all grid with -1, and end with 0
-    iterate using bellman, not makaing recursion, but using the previos genaratiiin board
-    after a large number of iterations, the values inside the grid will converge
-    the CORRECT_ACTION_PROB and GAMMA are inside grid_world'
-    ja existe a funcao transition_probability => p(s'|s, a)
-    ja existe a funcao reward => r(s, a)
-    ja existe get_valid_sucessors
-    """
+
     dimensions = grid_world.dimensions
     possible_actions = [STOP, UP, RIGHT, DOWN, LEFT]
     old_value = initial_value
@@ -280,14 +214,6 @@ def policy_iteration(grid_world, initial_value, initial_policy, evaluations_per_
     :rtype policy: tridimensional NumPy array.
     """
 
-    """
-    alterna entre (avaliacao de politica) e (aprimoramento de politica) 
-    o aprimoramento eh realizado atravws de tomar uma politica gulosa a partir da funcao valor da autal politirca ?? 
-    pode-se mostrar que esse alg converge pra uma politica otima
-    for:
-        intera chamando police evaluation e depois police improvement (greedy) até que convergimos para a melhor police e melhor value funciotn
-    intera ate convergir
-    """
     value = np.copy(initial_value)
     policy = np.copy(initial_policy)
 
